1f683c29f2eabed0d76f6446222bf278b8d3ec5d
Tobin Waters <tkwaterss@gmail.com>
2023-11-21 10:23:14 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import WhyClover from "./pages/WhyClover";
+import SuccessStories from "./pages/SuccessStories";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="why_clover" element={<WhyClover />} />
+          <Route path="success_stories" element={<SuccessStories />} />
+          <Route path="about" element={<About />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/WhyClover.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WhyClover = () => {
+  return (
+    <div>WhyClover</div>
+  )
+}
+
+export default WhyClover

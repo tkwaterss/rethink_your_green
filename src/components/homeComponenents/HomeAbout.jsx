1834979d21b313f260a32b91943992280529521f
Tobin Waters <tkwaterss@gmail.com>
2023-11-22 14:16:09 -0700

    about section draft

--- src/components/homeComponenents/HomeAbout.jsx
+++ src/components/homeComponenents/HomeAbout.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import classes from './HomeAbout.module.css';
+import React from "react";
+import classes from "./HomeAbout.module.css";
 
 const HomeAbout = () => {
   return (
-    <div>HomeAbout</div>
-  )
-}
+    <section className={classes.aboutSection}>
+      <h3>About Us</h3>
+      <p>
+        A short bio about the company . List some things about why the company
+        was started. What is the companies mission statement. Also list some
+        stuff about who the owner is and why he started this company Possibly
+        include a photo of Marcus
+      </p>
+      <p>
+        However much other stuff you want to include, blah alksf asldfk jakdf
+        haslkfj a asfdhasdkjg a;lgkj adhgsadkjg a kakh gakjdfh kajsdfh kjdsh
+        fkajh dflkajd glakjd adlskjfasd f \aaslfk aldk glk djs gldj aglk dasj
+        gslkd fjslkg jaroijeoautiaye fiau kh fiuah ruih aiufh arigh af askdj
+        fhiauh iua hiusiluialufh lskuzfh ziulfh lesUfhkszfhlkusDDF
+      </p>
+    </section>
+  );
+};
 
-export default HomeAbout
+export default HomeAbout;

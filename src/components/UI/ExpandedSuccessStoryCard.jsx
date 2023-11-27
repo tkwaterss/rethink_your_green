84f4dbc21eb4b4771ec06ad9193781d7fbe7a6da
Tobin Waters <tkwaterss@gmail.com>
2023-11-27 16:46:00 -0700

    started on Success Stories Page

---
+++ src/components/UI/ExpandedSuccessStoryCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ExpandedSuccessStoryCard = () => {
+  return (
+    <div>
+      <div>
+        <h1>Some Story Title</h1>
+        <p>Story Description</p>
+      </div>
+      <div>
+        Story Photo Here
+      </div>
+    </div>
+  );
+};
+
+export default ExpandedSuccessStoryCard;
